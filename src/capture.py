c8abc81f38b2a2e9662498e9b23541a261b68939
Nathan Joss (nat543207) <nate@jossfamily.com>
2015-11-09 17:27:20 -0600

    Changed default interpreter to Python2 for OpenCV; implemented code that captures image from camera and displays it to the screen.

--- src/capture.py
+++ src/capture.py
@@ -1,3 +1,22 @@
 """ Implements functions that save video buffer stored in memory to a file.
     Will probably use OpenCV funcitons/types to accomtypes to accomplish this.
 """
+import cv
+import cv2
+
+camera = cv2.VideoCapture()
+file_props = { "filename" : "out.vid",
+                 "fourcc" : int(camera.get(cv.CV_CAP_PROP_FOURCC)),
+                    "fps" : int(camera.get(cv.CV_CAP_PROP_FPS)),
+                   "size" : (int(camera.get(cv.CV_CAP_PROP_FRAME_WIDTH)), int(camera.get(cv.CV_CAP_PROP_FRAME_HEIGHT))), }
+
+def take_picture():
+    camera.open(0)
+    output = cv2.VideoWriter(file_props["filename"], file_props["fourcc"], file_props["fps"], file_props["size"])
+    tmp, frame = camera.read()
+    cv2.imshow("test", frame)
+    cv2.waitKey(0)
+#    output.write(frame)
+
+if __name__ == "__main__":
+    take_picture()
--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-#/user/bin/env python3
+#/user/bin/env python2
 """ Main file.  The program's subprocesses should be managed here.
     For now, the program just prints a message.
 """
+import capture
+
 def main():
-    print("Nothing to do, exiting...")
+    capture.take_picture()
 
 if __name__ == "__main__":
     main()
-
