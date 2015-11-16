6c0c57bc38311a5803b85332a981b3bea757a9a8
Nathan Joss (nat543207) <nate@jossfamily.com>
2015-11-16 17:55:51 -0600

    Video capture now triggers on motion, records to a file that can be played back by the latest stable version of VLC (not git version)

--- src/capture.py
+++ src/capture.py
@@ -1,51 +1,115 @@
 """ Implements functions that save video buffer stored in memory to a file.
     Will probably use OpenCV funcitons/types to accomtypes to accomplish this.
 """
-import cv
 import cv2
+from Queue import Queue
+from datetime import datetime, timedelta
+import motion
 
-camera = cv2.VideoCapture()
 file = {  "name" : "out.avi", # If this doesn't end in .avi, errors happen
-        "fourcc" : cv.CV_FOURCC(*"XVID"),
-           "fps" : 60,
-          "size" : (640, 480), 
+        "fourcc" : cv2.cv.CV_FOURCC(*"XVID"),
+           "fps" : 30,
+          "size" : (640, 480),
          "color" : True, }
+camera = cv2.VideoCapture()
+recording = cv2.VideoWriter()
+vidbuf = Queue()
 
 
-
-# Capture a single image from the default camera, display it,
-# and write it to a file.
-def take_picture():
-    camera.open(0)
-    if not camera.isOpened():
-        print "Error opening camera"
-
-    tmp, frame = camera.read()
-
-    cv2.imshow("test", frame)
-    cv2.imwrite("pic.jpg", frame)
-    cv2.waitKey(1000)
-    camera.release()
+class Frame:
+    def __init__(self, timestamp, image, has_motion):
+        self.timestamp = timestamp
+        self.image = image
+        self.has_motion = has_motion
 
 
-# Capture a video and write it to a file.
-#
-# Currently, the file can be written but seems to lack contents
-def record_video(duration):
+def capture():
+    success, img = camera.read()
+    return Frame(datetime.now(), img, motion.CV_detects_motion(img))
+
+
+def threshold(seconds):
+    return timedelta(0, seconds)
+
+
+def dump_buffer(buf, file):
+    if not file.isOpened():
+        return False
+    file.write(buf)
+
+
+def func():
+    bufsize = 60
     camera.open(0)
     output = cv2.VideoWriter(file["name"], file["fourcc"], file["fps"], file["size"], file["color"])
-    
-    for i in range(0,50):
-        tmp, frame = camera.read()
-        cv2.imshow("test", frame)
-        output.write(frame)
-        cv2.waitKey(1)
+    recording = False
 
-    camera.release()
-    output.release()
+    for i in range(0, 60):
+        vidbuf.put(capture())
+
+    while True:
+        frame = capture()
+        if frame.has_motion:
+            recording = True
+            while 0 < vidbuf.qsize():
+                output.write(vidbuf.get().image)
+            output.write(frame.image)
+        else: #if not frame.has_motion:
+            vidbuf.put(capture())
+            if bufsize < vidbuf.qsize():
+                vidbuf.get(True)
+                if recording == True:
+                    while 0 < vidbuf.qsize():
+                        output.write(vidbuf.get().image)
+                    output.close()
+                    recording = False
+                    break
+
+
+
+
+# # Capture a single image from the default camera, display it,
+# # and write it to a file.
+# def take_picture():
+#     camera.open(0)
+#     if not camera.isOpened():
+#         print "Error opening camera"
+#     tmp, frame = camera.read()
+#     cv2.imshow("test", frame)
+#     cv2.imwrite("pic.jpg", frame)
+#     cv2.waitKey(1000)
+#     camera.release()
+
+
+## Capture a video and write it to a file.
+##
+## Currently, the file can be written but seems to lack contents
+#def record_video(duration):
+#    camera.open(0)
+#    output = cv2.VideoWriter()
+#    for i in range(0,file["fps"]*duration):
+#        tmp, frame = camera.read()
+#        if not output.isOpened():
+#            output.open(file["name"], file["fourcc"], file["fps"], file["size"], file["color"])
+#            if not output.isOpened():
+#                print "Error opening output file"
+#                break
+##        if frame.empty():
+##            break
+##        print type(frame)
+##        cv2.imshow("test", frame)
+##        cv2.imwrite("out/%.3d.jpg" % i, frame)
+#        frame[0:i][0:50] = [0,255,0]
+#        output.write(frame)
+#        cv2.waitKey(1)
+#
+#    camera.release()
+#    output.release()
 
 
 
 if __name__ == "__main__":
-    take_picture()
-    record_video(5)
+    func()
+#    take_picture()
+#    record_video(1)
+
--- src/motion.py
+++ src/motion.py
@@ -6,15 +6,39 @@
     parallel (separate threads / python equivalent)
 """
 
+import cv2
+
 # Determine if the IR sensor on the Pi is detecting motion
 def IR_detects_motion():
     # Read from GPIO pin(s) to determine if IR sensor is triggered
     # Return result
     pass
 
+
+motion_detector = cv2.BackgroundSubtractorMOG()
 # Determine if there is motion in the frames using a computer vision algorithm
-def CV_detects_motion():
+def CV_detects_motion(frame):
     # Accept some arguments, probably background and captured image
     # Utilize one of OpenCV's vision algorithms, probably background subtraction
     # Return result (bool or movement mask, not sure which one yet)
-    pass
+
+    #Thanks to the user 'ravwojdyla' on StackOverflow for this Python
+    #alternative to static funciton variables
+    #http://stackoverflow.com/questions/279561
+    
+    #  Implement
+    
+    motion = motion_detector.apply(frame)
+    contours, h = cv2.findContours(motion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.imshow("Motion", motion)
+    cv2.waitKey(1)
+
+    threshold = 25
+    for c in contours:
+        if threshold**2 < cv2.contourArea(c):
+            return True;
+    return False
+
+if __name__ == "__main__":
+    camera = cv2.VideoCapture(0)
+    CV_detects_motion(camera.read()[1])
