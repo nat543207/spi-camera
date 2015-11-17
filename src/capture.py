415315aa4a0613f2f162e1943fc2d883fdb946cf
Nathan Joss (nat543207) <nate@jossfamily.com>
2015-11-16 22:02:49 -0600

    Cleaned up codebase (removed unnecessary functions and reorganized variables)

--- src/capture.py
+++ src/capture.py
@@ -6,15 +6,6 @@ from Queue import Queue
 from datetime import datetime, timedelta
 import motion
 
-file = {  "name" : "out.avi", # If this doesn't end in .avi, errors happen
-        "fourcc" : cv2.cv.CV_FOURCC(*"XVID"),
-           "fps" : 30,
-          "size" : (640, 480),
-         "color" : True, }
-camera = cv2.VideoCapture()
-recording = cv2.VideoWriter()
-vidbuf = Queue()
-
 
 class Frame:
     def __init__(self, timestamp, image, has_motion):
@@ -23,93 +14,39 @@ class Frame:
         self.has_motion = has_motion
 
 
-def capture():
+def capture_from(camera):
     success, img = camera.read()
     return Frame(datetime.now(), img, motion.CV_detects_motion(img))
 
 
-def threshold(seconds):
-    return timedelta(0, seconds)
-
-
-def dump_buffer(buf, file):
-    if not file.isOpened():
-        return False
-    file.write(buf)
-
-
-def func():
+def record_on_motion(filename, fourcc=cv2.cv.CV_FOURCC(*"XVID"), fps=30, size=(640,480), color=True):
+    vidbuf = Queue()
+    camera = cv2.VideoCapture()
     bufsize = 60
     camera.open(0)
-    output = cv2.VideoWriter(file["name"], file["fourcc"], file["fps"], file["size"], file["color"])
+    output = cv2.VideoWriter(filename, fourcc, fps, size, color)
     recording = False
 
-    for i in range(0, 60):
-        vidbuf.put(capture())
+    for i in range(0, bufsize):
+        vidbuf.put(capture_from(camera))
 
     while True:
-        frame = capture()
+        frame = capture_from(camera)
         if frame.has_motion:
             recording = True
             while 0 < vidbuf.qsize():
                 output.write(vidbuf.get().image)
             output.write(frame.image)
         else: #if not frame.has_motion:
-            vidbuf.put(capture())
+            vidbuf.put(capture_from(camera))
             if bufsize < vidbuf.qsize():
                 vidbuf.get(True)
                 if recording == True:
                     while 0 < vidbuf.qsize():
                         output.write(vidbuf.get().image)
-                    output.close()
                     recording = False
                     break
 
 
-
-
-# # Capture a single image from the default camera, display it,
-# # and write it to a file.
-# def take_picture():
-#     camera.open(0)
-#     if not camera.isOpened():
-#         print "Error opening camera"
-#     tmp, frame = camera.read()
-#     cv2.imshow("test", frame)
-#     cv2.imwrite("pic.jpg", frame)
-#     cv2.waitKey(1000)
-#     camera.release()
-
-
-## Capture a video and write it to a file.
-##
-## Currently, the file can be written but seems to lack contents
-#def record_video(duration):
-#    camera.open(0)
-#    output = cv2.VideoWriter()
-#    for i in range(0,file["fps"]*duration):
-#        tmp, frame = camera.read()
-#        if not output.isOpened():
-#            output.open(file["name"], file["fourcc"], file["fps"], file["size"], file["color"])
-#            if not output.isOpened():
-#                print "Error opening output file"
-#                break
-##        if frame.empty():
-##            break
-##        print type(frame)
-##        cv2.imshow("test", frame)
-##        cv2.imwrite("out/%.3d.jpg" % i, frame)
-#        frame[0:i][0:50] = [0,255,0]
-#        output.write(frame)
-#        cv2.waitKey(1)
-#
-#    camera.release()
-#    output.release()
-
-
-
 if __name__ == "__main__":
-    func()
-#    take_picture()
-#    record_video(1)
-
+    record_on_motion(str(datetime.now()) + ".avi")
