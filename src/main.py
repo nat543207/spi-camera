4befd5b6a7dccb80f0760d1a841e3a195ecf93f6
Nathan Joss (nat543207) <nate@jossfamily.com>
2015-11-19 17:13:13 -0600

    Fully implemented main routine (runs until stopped)

--- src/main.py
+++ src/main.py
@@ -2,11 +2,22 @@
 """ Main file.  The program's subprocesses should be managed here.
     For now, the program just prints a message.
 """
+import os
 import capture
 
 # Execute main loop.  Not very interesting.  Move along now.
 def main():
-    capture.take_picture()
+    # Thanks to StackOverflow users Blair Conrad and jesterjunk
+    # for how to make missing directories!
+    # http://stackoverflow.com/questions/273192
+    output_dir = "./captures"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+#    GPIO.setmode(GPIO.BOARD)
+#    GPIO.setup(3, GPIO.IN)
+    while True:
+        capture.record_on_motion(output_dir)
 
 
 
